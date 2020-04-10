c0338c8a8afceb49fd5c9ca725dcf2b1e629c1e5
Connor Ford <cbfa2016@mymail.pomona.edu>
2020-04-09 23:33:59 -0700

    verify that vertices are in stream before edding edges

--- library/SimplexStream.hs
+++ library/SimplexStream.hs
@@ -13,20 +13,35 @@ instance Show Simplex where
     show (Vertex a) = show a
     show (Edge a b) = "(" ++ show a ++ "," ++ show b ++ ")"
 
-data Stream = EmptyStream | Simplicies [Simplex]
+data Stream = Simplicies [Simplex]
 
 instance Show Stream where
-    show EmptyStream = ""
     show (Simplicies []) = ""
     show (Simplicies (x:xs)) = show x ++ ", " ++ show (Simplicies xs)
 
 initializeStream :: Stream 
-initializeStream = EmptyStream
+initializeStream = Simplicies []
 
 addVertex :: Stream -> Int -> Stream 
-addVertex EmptyStream x = Simplicies [Vertex x]
 addVertex (Simplicies xs) x = Simplicies $ (Vertex x):xs
 
+-- Returns True if the vertex is in the stream. Otherwise, False.
+isVertexInStream :: Stream -> Int -> Bool
+isVertexInStream (Simplicies [])   _ = False
+isVertexInStream (Simplicies (x:xs)) v = 
+    case x of
+        Vertex z -> if z == v then True else isVertexInStream (Simplicies xs) v
+        Edge a b -> isVertexInStream (Simplicies xs) v
+
+
+-- if a vertex on the edge is not in the stream, you get the original stream returned.
 addEdge :: Stream -> Int -> Int -> Stream 
-addEdge EmptyStream a b = Simplicies [Edge a b]
-addEdge (Simplicies xs) a b = Simplicies $ (Edge a b):xs
+addEdge stream@(Simplicies xs) a b = 
+    let
+        aInStream = isVertexInStream stream a
+        bInStream = isVertexInStream stream b
+    in
+        if aInStream && bInStream then
+            Simplicies $ (Edge a b):xs
+        else 
+            stream
